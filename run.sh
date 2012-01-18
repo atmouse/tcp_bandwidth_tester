7de700c8acd7655490dbc5b6dc9bf259f03b200c
atmouse <arne.esterhuizen@gmail.com>
2012-01-18 23:59:53 +0200

    changed run.sh - now performs dirty hack

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #<src ip> <dest ip> <port> <congestion> <filename> <network interface> <num_packets>
-python tcpclient.py "$2" "$3" "$4" &
 gcc rtt4.c -o rtt4 -lpcap
 ./rtt4 "$6" "$1" "$2" "$7" "`basename $5`_`basename $4`_`basename $1`_rtt.txt" &
+python tcpclient.py "$2" "$3" "$4"
+pkill -9 rtt4
+exit 0
