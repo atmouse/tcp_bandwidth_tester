0deb56136837b05a2906b25dfab939dc6d3bd60b
atmouse <arne.esterhuizen@gmail.com>
2012-01-19 10:09:38 +0200

    changed run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #<src ip> <dest ip> <port> <congestion> <filename> <network interface> <num_packets>
 gcc rtt4.c -o rtt4 -lpcap
-./rtt4 "$6" "$1" "$2" "$7" "`basename $5`_`basename $4`_`basename $1`_rtt.txt" &
+./rtt4 "$6" "$1" "$2" "$7" "`basename $5`_`basename $4`-`basename $1`-rtt.txt" &
 python tcpclient.py "$2" "$3" "$4"
 pkill -9 rtt4
 exit 0
